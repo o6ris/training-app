799c9e92bc7bf23287dcc38ce6421a878053f9e1
o6ris <loic.tsiry@gmail.com>
2023-11-19 13:33:18 +0100

    fix(api: route exercise): adapt post to add muscle name and get to populate muscle with its name too

--- app/api/exercise/route.js
+++ app/api/exercise/route.js
@@ -1,13 +1,24 @@
 import Exercise from "@modules/server/models/exercise";
+import Muscle from "@modules/server/models/muscle";
 import connectDb from "@lib/mongodb";
 import { NextResponse } from "next/server";
+import { models } from "mongoose";
 
 export async function POST(request) {
   try {
-    const exercise = await request.json();
+    const body = await request.json();
     await connectDb();
+    const muscle = await Muscle.findById(body.muscle);
+    const exercise = new models.Exercise({
+      muscle: muscle,
+      name: body.name,
+    });
     const newExercise = await Exercise.create(exercise);
-    return NextResponse.json(newExercise, { message: "Exercise Created" }, { status: 201 });
+    return NextResponse.json(
+      newExercise,
+      { message: "Exercise Created" },
+      { status: 201 }
+    );
   } catch (err) {
     const { errors } = err;
     return NextResponse.json(errors, { status: 400 });
@@ -15,18 +26,31 @@ export async function POST(request) {
 }
 
 export async function GET(request, { params }) {
-  
   try {
-    const muscle_id = request?.url.split('=')[1]
+    const muscle_id = request?.url.split("=")[1];
     await connectDb();
     const findExercises = async () => {
       if (muscle_id) {
-        return await Exercise.find({ muscle: muscle_id });
+        console.log(muscle_id);
+        return Exercise.find({ muscle: muscle_id });
       }
-      return await Exercise.find();
-    }
+      return Exercise.find();
+    };
+    console.log("before", await findExercises());
     const exercises = await findExercises();
-    return NextResponse.json(exercises, { status: 200 });
+    const populatedExercises = [];
+    for (const exercise of exercises) {
+      const populatedExercise = await exercise
+        .populate({
+          path: "muscle",
+          select: "name",
+        })
+      populatedExercises.push(populatedExercise);
+      console.log("populatedExercise", populatedExercise);
+    }
+
+    console.log("after", populatedExercises);
+    return NextResponse.json(populatedExercises, { status: 200 });
   } catch (err) {
     const { errors } = err;
     return NextResponse.json(errors, { status: 404 });
