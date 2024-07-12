e22adbb884b88eaee5019edf5c588154f189d4ae
o6ris <loic.tsiry@gmail.com>
2024-07-12 20:03:29 +0200

    feat(stats page): display stats and adapt style

--- app/stats/page.jsx
+++ app/stats/page.jsx
@@ -17,6 +17,8 @@ function Stats() {
 
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
+  const getMinutes = (seconds) => Math.floor(seconds / 60);
+  const getSeconds = (seconds) => seconds % 60;
   // console.log("stats", stats)
   return (
     <Accordion
@@ -26,24 +28,60 @@ function Stats() {
       className={classes.accordion}
     >
       {Object.keys(stats).map((exerciseName, i) => {
-        // console.log("exerciseName", exerciseName);
+        // console.log("exercise", stats[exerciseName][0]);
         const key = (i + 1).toString();
         return (
           <AccordionItem
             key={key}
             title={
-              <div className={classes.accordion_title}>
-                <div>
-                  <h3>{exerciseName.toUpperCase()}</h3>
-                  <span>{formatDate(stats[exerciseName][0].date, false)}</span>
-                </div>
-                {/* <span>{getMinutes(i).toString().padStart(2, "0")}</span>:
-                <span>{getSeconds(i).toString().padStart(2, "0")}</span> */}
+              <div className={classes.title_wrapper}>
+                <h3>{exerciseName.toUpperCase()}</h3>
+                <span>{formatDate(stats[exerciseName][0].date, false)}</span>
               </div>
             }
             classNames={{ base: classes.accordion_item }}
           >
-            <div className={classes.session_container}></div>
+            <div className={classes.session_container}>
+              <div className={classes.data_wrapper}>
+                <div className={`${classes.data} ${classes.sets}`}>
+                  <p className={classes.data_value}>
+                    {stats[exerciseName][0].sets.length}
+                  </p>
+                  <p className={classes.data_title}>Sets</p>
+                </div>
+                <div className={`${classes.data} ${classes.reps}`}>
+                  <p className={classes.data_value}>
+                    {" "}
+                    {stats[exerciseName][0].sets.reduce(
+                      (sum, current) => sum + current.reps,
+                      0
+                    )}
+                  </p>
+                  <p className={classes.data_title}>Reps</p>
+                </div>
+                <div className={`${classes.data} ${classes.rm}`}>
+                  <p className={classes.data_value}>
+                    {stats[exerciseName][0].sets.reduce(
+                      (sum, current) => sum + current.reps*current.weight,
+                      0
+                    )} KG
+                  </p>
+                  <p className={classes.data_title}>Volume</p>
+                </div>
+                <div className={`${classes.data} ${classes.rest_time}`}>
+                  <p className={classes.data_value}>{`${getMinutes(
+                    stats[exerciseName][0].rest_time
+                  )}:${getSeconds(stats[exerciseName][0].rest_time)}`}</p>
+                  <p className={classes.data_title}>Rest time</p>
+                </div>
+                <div className={`${classes.data} ${classes.training_time}`}>
+                  <p className={classes.data_value}>{`${getMinutes(
+                    stats[exerciseName][0].training_time
+                  )}:${getSeconds(stats[exerciseName][0].training_time)}`}</p>
+                  <p className={classes.data_title}>Training time</p>
+                </div>
+              </div>
+            </div>
           </AccordionItem>
         );
       })}
