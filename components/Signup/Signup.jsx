170b7f79ac3b6c3af66e93326b634ff4d21246a5
o6ris <loic.tsiry@gmail.com>
2024-09-28 11:25:48 +0200

    feat(useuser, signup): create signup page and feature, confirmed password

--- components/Signup/Signup.jsx
+++ components/Signup/Signup.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import classes from "./signup.module.css";
+import useUser from "@modules/client/userRequests/useUser";
+import InputField from "@core/ui/Fields/InputField/InputField";
+import BasicButton from "@core/ui/Button/BasicButton";
 
 function Signup() {
+  const { addUser } = useUser();
+  const [credentials, setCredentials] = useState({
+    password: "",
+    confirmedPassword: "",
+  });
+  const [disabledButton, setDisabledButton] = useState(true);
+  const handleOnChange = (name, value) => {
+    const t = { ...credentials };
+    t[name] = value;
+    setCredentials(t);
+  };
+
+  useEffect(() => {
+    if (
+      credentials?.password === credentials?.confirmedPassword &&
+      (credentials?.password !== "" || credentials?.confirmedPassword !== "")
+    ) {
+      setDisabledButton(false);
+    } else {
+      setDisabledButton(true);
+    }
+  }, [credentials]);
+
   return (
-    <div>Signup</div>
-  )
+    <div className={classes.container}>
+      <InputField
+        label="Email"
+        variant="bordered"
+        placeholder="john.doe@mail.com"
+        labelPlacement="outside"
+        onChange={(value) => handleOnChange("email", value)}
+      />
+      <InputField
+        label="Password"
+        variant="bordered"
+        placeholder=""
+        labelPlacement="outside"
+        onChange={(value) => handleOnChange("password", value)}
+      />
+      <InputField
+        label="Confirm Password"
+        variant="bordered"
+        placeholder=""
+        labelPlacement="outside"
+        onChange={(value) => handleOnChange("confirmedPassword", value)}
+      />
+      <BasicButton
+        onAction={() => addUser(credentials)}
+        buttonContent={"Sign up"}
+        buttonStyle={classes.signup_button}
+        isDisabled={disabledButton}
+      />
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
--- modules/client/userRequests/useUser.js
+++ modules/client/userRequests/useUser.js
@@ -21,11 +21,34 @@ export default function useUser(userSession) {
     }
   };
 
+  const addUser = async (body) => {
+    try {
+      const url = `${baseUrl}/api/users`;
+      delete body.confirmedPassword;
+      console.log("body", body);
+      const response = await fetch(
+        url,
+        {
+          method: "POST",
+          body: JSON.stringify(body),
+        },
+        { next: { revalidate: 10 } }
+      );
+      if (response) {
+        const user = await response.json();
+        console.log("user", user);
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+
   useEffect(() => {
     getUser();
   }, [userSession]);
 
   return {
     userId,
+    addUser,
   };
 }
