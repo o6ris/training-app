70beec4ce3d0c38ff5e7a3fe4d10ed6d7b3062da
o6ris <loic.tsiry@gmail.com>
2025-02-11 11:42:38 -0700

    add metadata on home, change logo and add favicon

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -6,6 +6,32 @@ import Hero from "@components/Home/Hero";
 import Benefits from "@components/Home/Benefits";
 import Chart from "@components/Home/Chart";
 
+export const metadata = {
+  title: "GrindPAL | Track your fitness progress",
+  description: "Track your workouts at the gym and monitor your progress with GrindPAL.",
+  keywords: "workout, fitness, strength training, progress tracker, gym",
+  openGraph: {
+    title: "GrindPAL | Track your fitness progress",
+    description: "Track your workouts at the gym and monitor your progress with GrindPAL.",
+    url: "https://grind-pal.vercel.app",
+    siteName: "GrindPAL",
+    // images: [
+    //   {
+    //     url: "/cover.jpg", 
+    //     width: 1200,
+    //     height: 630,
+    //   },
+    // ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "GrindPAL | Track your fitness progress",
+    description: "Track your workouts at the gym and monitor your progress with GrindPAL.",
+    // images: ["/cover.jpg"],
+  },
+};
+
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
