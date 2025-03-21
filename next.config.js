e0e97caddb254c9f8168b7944ef4489e4cba9485
o6ris <loic.tsiry@gmail.com>
2025-03-20 18:06:45 -0600

    transform my web app to a PWA

--- next.config.js
+++ next.config.js
@@ -1,15 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  scope: "/app",
+  sw: "service-worker.js",
+  disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  output: "standalone",
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/home',
+        source: "/",
+        destination: "/home",
         permanent: true,
       },
     ];
   },
-}
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
