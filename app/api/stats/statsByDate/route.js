489b0bf7a0fb6b32bfdfb63294bbecc641dd1641
o6ris <loic.tsiry@gmail.com>
2025-03-22 18:58:51 -0600

    feat(route statsByDate): check if user Exist

--- app/api/stats/statsByDate/route.js
+++ app/api/stats/statsByDate/route.js
@@ -2,17 +2,27 @@ import Stats from "@modules/server/models/stats";
 import Exercise from "modules/server/models/exercise";
 import User from "@modules/server/models/user";
 import connectDb from "@lib/mongodb";
+import checkId from "modules/server/utils/checkId";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
   const user = request.nextUrl.searchParams.get("user");
   const date = request.nextUrl.searchParams.get("date");
 
-  console.log("date", new Date(date));
-  console.log("user", user);
-
   try {
     await connectDb();
+
+    const profileExist = await User.exists({ _id: user });
+    
+    if (!checkId(user)) {
+      throw { message: "Wrong id", status: 500 };
+    }
+    if (!profileExist) {
+      throw {
+        message: `User with ID ${user} does not exist`,
+        status: 400,
+      };
+    }
 
     const startOfDay = new Date(date);
     startOfDay.setUTCHours(0, 0, 0, 0);
