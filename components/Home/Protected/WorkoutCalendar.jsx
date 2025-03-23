def3c72e18a5cbcdafbf77d9f335b507dbef68b2
o6ris <loic.tsiry@gmail.com>
2025-03-22 18:23:36 -0600

    feat(WorkoutCalendar): create calendar component (UI) and displayed it on home when connected

---
+++ components/Home/Protected/WorkoutCalendar.jsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import classes from "./workoutCalendar.module.css"
+import { Calendar } from "@heroui/react";
+import { today, getLocalTimeZone } from "@internationalized/date";
+
+function WorkoutCalendar() {
+  const [value, setValue] = useState(today(getLocalTimeZone()));
+  const [calendarWidth, setCalendarWidth] = useState(getInitialWidth());
+
+  console.log("value", value)
+
+  function getInitialWidth() {
+    return window.innerWidth < 512 ? "100" : "400px";
+  }
+
+  useEffect(() => {
+    function handleResize() {
+      setCalendarWidth(window.innerWidth < 600 ? "100" : "400px");
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <Calendar
+      aria-label="Date (Controlled)"
+      value={value}
+      onChange={setValue}
+      maxValue={today(getLocalTimeZone())}
+      calendarWidth={calendarWidth}
+      classNames={{
+        base: classes.calendar_wrapper,
+        headerWrapper: classes.calendar_header,
+        title: classes.calendar_title,
+        gridHeaderRow: classes.calendar_header_row,
+        gridHeaderCell: classes.calendar_header_cells,
+        prevButton: classes.calendar_prev_button,
+        nextButton: classes.calendar_next_button,
+        content: classes.calendar_content,
+        gridBodyRow: classes.calendar_body_row
+      }}
+    />
+  );
+}
+
+export default WorkoutCalendar;
