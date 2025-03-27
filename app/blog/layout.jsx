af01d0e57d7177e826008c09559d934c82ec0cd7
o6ris <loic.tsiry@gmail.com>
2025-03-27 16:39:52 -0600

    feat(Header): keep header in blog page

--- app/blog/layout.jsx
+++ app/blog/layout.jsx
@@ -1,7 +1,16 @@
 import classes from "./blog.module.css";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import Header from "@components/Home/Header";
 
 async function BlogLayout({ children }) {
-  return <div className={classes.layout}>{children}</div>;
+  const session = await getServerSession(authOptions);
+  return (
+    <div className={classes.layout}>
+      <Header session={session} />
+      {children}
+    </div>
+  );
 }
 
 export default BlogLayout;
--- components/Home/Header.jsx
+++ components/Home/Header.jsx
@@ -5,21 +5,27 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@heroui/react";
 import { signOut } from "next-auth/react";
+import { usePathname } from "next/navigation";
 
 function Header({ session }) {
+  const pathname = usePathname();
+
   return (
     <header className={classes.header}>
-      <Image
-        src="/logo-white.svg"
-        alt="GrindPal Logo"
-        width={150}
-        height={150}
-        priority
-      />
+      <Link href={"/"}>
+        <Image
+          src={pathname.includes("home") ? "/logo-white.svg" : "/logo.svg"}
+          alt="GrindPal Logo"
+          width={150}
+          height={150}
+          priority
+        />
+      </Link>
       <div className={classes.button_wrapper}>
         {session ? (
           <Button
             className={classes.link}
+            style={{ color: pathname.includes("home") ? "" : "#02091c" }}
             onPress={() => signOut({ callbackUrl: "/home" })}
           >
             Logout
