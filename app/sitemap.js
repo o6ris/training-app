112c594d1f618d773c503c70474996ea65112766
o6ris <loic.tsiry@gmail.com>
2025-03-28 11:02:09 -0600

    feat: sitemap creation

---
+++ app/sitemap.js
@@ -0,0 +1,48 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const blogDirectory = path.join(process.cwd(), "content/posts");
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
+function getAllPosts() {
+  const fileNames = fs.readdirSync(blogDirectory);
+  const posts = fileNames.map((fileName) => {
+    const fullPath = path.join(blogDirectory, fileName);
+    const fileContent = fs.readFileSync(fullPath, "utf-8");
+    const { data } = matter(fileContent);
+
+    return {
+      url: fileName.replace(/\.md$/, ""),
+      lastModified: new Date(data.date),
+    };
+  });
+
+  posts.sort((a, b) => b.date - a.date);
+  return posts;
+}
+
+export default function sitemap() {
+  try {
+    if (!baseUrl) {
+      throw new Error("NEXT_PUBLIC_API_URL is not set.");
+    }
+
+    const posts = getAllPosts();
+
+    return [
+      {
+        url: `${baseUrl}`,
+        lastModified: new Date(),
+      },
+      {
+        url: `${baseUrl}/blog`,
+        lastModified: new Date(),
+      },
+      ...posts,
+    ];
+  } catch (error) {
+    console.error("Error generating sitemap:", error);
+    return []; 
+  }
+}
