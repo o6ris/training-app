21f39e3b145c98fcd1c9fc7c264d7cb63ddd2b54
o6ris <loic.tsiry@gmail.com>
2025-04-03 16:54:35 -0600

    feat(all login pages): can go back to login page from forgotpassword and signup page

--- components/Signup/Signup.jsx
+++ components/Signup/Signup.jsx
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
-// import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 import classes from "./signup.module.css";
 import useUser from "@modules/client/requests/useUser";
 import InputField from "@core/ui/Fields/InputField/InputField";
 import BasicButton from "@core/ui/Button/BasicButton";
-// import PopupButton from "@core/ui/Button/PopupButton";
 import Icon from "@core/ui/Icons/Icon";
 
 function Signup() {
@@ -135,45 +135,23 @@ function Signup() {
         buttonStyle={classes.signup_button}
         isDisabled={disabledButton}
       />
-      {/* <PopupButton
-        triggerButtonContent={"Get Whitelisted"}
-        buttonStyle={`${classes.signup_button} ${classes.whitelisted_button}`}
-        title={"Why join the Whitelist?"}
-        confirmButton="Join!"
-        onConfirm={() => router.push("https://forms.gle/HPNRcuLnmLnvMBwo6")}
-        autoOpen={true}
-        content={
-          <div className={classes.whilisted_modal_content}>
-            <p>
-              This version of the app is a{" "}
-              <strong>V0, an early MVP (Minimum Viable Product)</strong>. That
-              means it&apos;s still in <strong>testing mode</strong>, and I need a
-              small group of users to help shape it before a full release.
-            </p>
-            <p>
-              By getting whitelisted, you&apos;ll be part of an{" "}
-              <strong>exclusive group of early testers</strong> who will:
-            </p>
-            <ul>
-              <li>
-                ✅ <strong>Try out new features</strong> before anyone else
-              </li>
-              <li>
-                ✅ <strong>Give feedback</strong> on what works and what
-                doesn&apos;t
-              </li>
-              <li>
-                ✅ <strong>Report bugs</strong> and suggest improvements
-              </li>
-            </ul>
-            <p>
-              Your insights will be <strong>crucial</strong> in making the app
-              better. If you&apos;re interested in testing and shaping the
-              future of this app, 🚀
-            </p>
-          </div>
+      OR
+      <BasicButton
+        onAction={() => signIn("google")}
+        buttonContent={"Sign In with "}
+        buttonStyle={classes.login_google}
+        endContent={
+          <Image
+            src="/google-icon.png"
+            width={18}
+            height={18}
+            alt="Picture of the author"
+          />
         }
-      /> */}
+      />
+      <div className={classes.go_back_button}>
+        <Link href="/login">Back to login</Link>
+      </div>
     </div>
   );
 }
